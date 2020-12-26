0155fdc7f414d09cef91f28c748e6d3a0759c95f
fmaced1 <fabricio.macedo.br@gmail.com>
2020-12-26 18:25:37 -0300

    rebuilding site Sáb 26 Dez 2020 18:25:37 -03

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,22 @@
 #!/bin/sh
 
+#git clone https://github.com/adityatelange/hugo-PaperMod themes/hugo-PaperMod --depth=1
 #git submodule add -b main https://github.com/fmaced1/fmced1.github.io.git public
 
 # If a command fails then the deploy stops
 set -e
+
+rm -rf ./public/* ./docs/*
 
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
 # Build the project.
 hugo -t hugo-PaperMod # if using a theme, replace with `hugo -t <YOURTHEME>`
 
-# Go To Public folder
-cp -R ./public/* ../
+mv ./public/ ./docs/
 
 # Add changes to git.
-git add .
+git add *
 
 # Commit changes.
 msg="rebuilding site $(date)"
