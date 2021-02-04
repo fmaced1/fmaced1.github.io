f730253aebdb774b72966b86c3a0d25ba7da7ea4
fmaced1 <fmaced@gmail.com>
2021-02-04 14:57:28 -0200

    rebuilding site Thu Feb  4 14:57:28 -02 2021

--- chama.py
+++
@@ -1,9 +0,0 @@
-import yfinance as yf
-
-ticker = "VALE3.SA"
-period = "1y"
-interval = "1wk"
-
-historical_data = yf.Ticker(ticker).history(period, interval, actions=False).dropna()
-
-print(historical_data.head())
---
+++ install-hugo.sh
@@ -0,0 +1,6 @@
+
+# Install brew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install hugo
+brew install hugo
